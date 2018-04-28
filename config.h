ee2b2457c6dc9747e79534b771bca2416a83e284
bitbrain-za <phil@bitbrain.co.za>
2018-04-28 15:00:06 +0200

    working

--- config.h
+++ config.h
@@ -4,16 +4,43 @@
 
 #define SYS_TICK 1 	/* milliseconds */
 
-/* Relay Pins */
-#define LIGHT1_G 	1	
-#define LIGHT1_O 	1
-#define LIGHT1_R 	1
-#define LIGHT2_G 	1
-#define LIGHT2_O 	1
-#define LIGHT2_R 	1
+#define D2 32
+#define D3 1
+#define D4 2
+#define D5 9
+#define D6 10
+#define D7 11
+#define D8 12
+#define D9 13
 
-#define ARROWG 		1
-#define ARROWO 		1
+// #define RELAY_1 D2
+// #define RELAY_2 D3
+// #define RELAY_3 D4
+// #define RELAY_4 D5
+// #define RELAY_5 D6
+// #define RELAY_6 D7
+// #define RELAY_7 D8
+// #define RELAY_8 D9
+
+#define RELAY_1 2
+#define RELAY_2 3
+#define RELAY_3 4
+#define RELAY_4 5
+#define RELAY_5 6
+#define RELAY_6 7
+#define RELAY_7 8
+#define RELAY_8 9
+
+/* Relay Pins */
+#define LIGHT1_G 	RELAY_1
+#define LIGHT1_O 	RELAY_2
+#define LIGHT1_R 	RELAY_3
+#define LIGHT2_G 	RELAY_6
+#define LIGHT2_O 	RELAY_7
+#define LIGHT2_R 	RELAY_8
+
+#define ARROWG 		RELAY_4
+#define ARROWO 		RELAY_5
 
 /* Robot Timing */
 #define GO_TIME_BASE  		5000
